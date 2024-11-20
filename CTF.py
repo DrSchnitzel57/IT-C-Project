2d1260c070dce36169fd2446eceeac1bfd0edc9c
Charlie Blodgett <83051957+DrSchnitzel57@users.noreply.github.com>
2024-11-20 16:57:12 -0700

    Commit

--- CTF.py
+++ CTF.py
@@ -1,1 +1,12 @@
+import time
 
+def print_slow(string):
+    for char in string:
+        print(char)
+        time.sleep(.25)
+
+def main():
+    print_slow("Welcome to my CTF challenge. You will be asked to do OSINT on a popular figure. As you obtain this information, you can enter it into the terminal, and will be given an encoded section of the flag.")
+
+if __name__ == "__main__":
+    main()
