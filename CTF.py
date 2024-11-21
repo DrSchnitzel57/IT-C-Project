9c75843a6ebe5ce5840ff37406d4f96ae13b9d78
Charlie Blodgett <83051957+DrSchnitzel57@users.noreply.github.com>
2024-11-21 00:02:41 +0000

    Commit

--- CTF.py
+++ CTF.py
@@ -1,12 +1,13 @@
 import time
-
-def print_slow(string):
+import sys
+def delay_print(string):
     for char in string:
-        print(char)
-        time.sleep(.25)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.01)
 
 def main():
-    print_slow("Welcome to my CTF challenge. You will be asked to do OSINT on a popular figure. As you obtain this information, you can enter it into the terminal, and will be given an encoded section of the flag.")
-
+    delay_print("Welcome to my CTF challenge. You will be asked to do OSINT on a popular figure. As you obtain this information, you can enter it into the terminal, and will be given an encoded section of the flag.")
+    
 if __name__ == "__main__":
     main()
