7fd727aea37cb339aeed4e67ede5385b2a53c0ea
Charlie Blodgett <83051957+DrSchnitzel57@users.noreply.github.com>
2024-11-25 15:32:04 -0700

    Finished

--- CTF.py
+++ CTF.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import re
 
 def delay_print(string):
     for char in string:
@@ -11,7 +12,7 @@ def delay_print(string):
 def ask(question, answer):
     delay_print(question)
     user_answer = input("> ")
-    if user_answer == answer:
+    if answer.match(user_answer):
         return True
     else:
         return False
@@ -19,7 +20,7 @@ def ask(question, answer):
 def main():
     delay_print("Welcome to my CTF challenge. You will be asked to do OSINT on a popular figure. As you obtain this information, you can enter it into the terminal, and will be given an encoded section of the flag.")
     question1 = "What is Bill Gates Address?"
-    answer1 = "1835 73rd Ave NE, Medina, Washington, USA"
+    answer1 = re.compile(r"1835\s?73rd\s?Ave\s?NE,?(\s?Medina,?\s?Washington,?\s?USA)?", re.IGNORECASE)
     loop1 = ask(question1, answer1)
     while loop1 is False:
         delay_print("Try Again!")
@@ -28,7 +29,7 @@ def main():
     time.sleep(0.2)
 
     question2 = "How many kids does he have?"
-    answer2 = "3"
+    answer2 = re.compile(r"3|(three)", re.IGNORECASE)
     loop2 = ask(question2, answer2)
     while loop2 is False:
         delay_print("Try Again!")
@@ -37,7 +38,7 @@ def main():
     time.sleep(0.2)
 
     question3 = "What is Bill Gates favorite Dessert?"
-    answer3 = "Cookies"
+    answer3 = re.compile(r"(toll)?\s?(house)?\s?cookies?", re.IGNORECASE)
     loop3 = ask(question3, answer3)
     while loop3 is False:
         delay_print("Try Again!")
@@ -46,7 +47,7 @@ def main():
     time.sleep(0.2)
 
     question4 = "What's his nickname?"
-    answer4 = "Trey"
+    answer4 = re.compile(r"trey", re.IGNORECASE)
     loop4 = ask(question4, answer4)
     while loop4 is False:
         delay_print("Try Again!")
@@ -55,14 +56,14 @@ def main():
     time.sleep(0.2)
     
     question5 = "Enter the flag here"
-    answer5 = "itc101{BILL_GATES_LOLLLLLLLLLLLLLL}"
+    answer5 = re.compile(r"itc101{BILL_GATES_LOLLLLLLLLLLLLLL}")
     loop5 = ask(question5, answer5)
     while loop5 is False:
         delay_print("Try Again!")
         loop5 = ask(question5, answer5)
     delay_print("Correct!!! Here is your reward:")
-    time.sleep(0.2)
     delay_print("https://youtu.be/c6DbXIoSxaM")
+    time.sleep(10)
  
 
 if __name__ == "__main__":
